bddf9842536fb76b64f48099bd35626229a99109
Simon Ermolinsky <gfromsx@gmail.com>
2024-02-08 20:39:46 +0300

    fix

---
+++ data.js
@@ -0,0 +1,10 @@
+export let data = [
+    {
+        picture: './media/amur.png',
+        autor: 'Марсель Руссо',
+        name: 'Охота Амура',
+        material: 'Холст, масло (50х80)',
+        price: '14 500 руб',
+        country: 'france'
+    }
+]
